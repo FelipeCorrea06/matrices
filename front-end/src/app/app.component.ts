741c92f2d39e9d967f4f91a068a55768556491bf
softpymesFelipe <lmelo@softpymes.com.co>
2019-09-18 17:51:35 -0500

    feat: 🎸 Implementacion de la vista
    Implementacion de la vista - se agregaron las funciones de agregar filas
    de matrices y creacion del objeto principal donde se almacenaran las
    matrices

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,10 +1,109 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: [ './app.component.css' ]
 })
 export class AppComponent {
-  title = 'front-end';
+	items: MenuItem[];
+	activeItem: MenuItem;
+
+	ngOnInit() {
+		this.items = [
+			{ id: '0', label: 'Matrices', icon: 'fa fa-fw fa-th' },
+			{ id: '1', label: 'Documentación', icon: 'fa fa-fw fa-book' },
+			{ id: '2', label: 'Información del Sistema', icon: 'fa fa-fw fa-info-circle' },
+			{ id: '3', label: 'Acerca De', icon: 'fa fa-fw fa-address-card' }
+		];
+
+		this.activeItem = this.items[0];
+	}
+
+	visible = 0;
+	valFila = [];
+	matriz = [ [] ];
+	matrices = [];
+
+	student = [
+		{
+			name: 'Luis Felipe Melo Delgado',
+			profession: 'Analista y Desarrollador',
+			description:
+				'Analista y desarrollador de software, amante de los deportes acuaticos, nadador olympico en charcos y lavaderos.. fiel amante de las tortillas de doña ama!!!',
+			photo: 'pipe.jpg',
+			job: 'Front-End',
+			social: '@InfernalRec',
+			red: 'Twitter'
+		},
+		{
+			name: 'Luis Felipe Correa Reina',
+			profession: 'Analista Funcional (El vigilante, el de los tintos,etc...)',
+			description: '',
+			photo: '',
+			job: 'Back-End',
+			social: '@pipeespipe97',
+			red: 'Instagram'
+		},
+		{
+			name: 'José Carlos Avila',
+			profession: 'Auxiliar de Sistemas',
+			description: 'Hermano de PHP',
+			photo: '',
+			job: 'Back-End',
+			social: 'jcavila44@misena.edu.co',
+			red: 'E-mail'
+		}
+	];
+
+	x = 1;
+	y = 1;
+
+	viewData(num: string) {
+		this.visible = parseInt(num, 10);
+	}
+
+	addRow() {
+		this.y = this.matriz[0].length;
+		let newRow = this.valFila;
+
+		let long = newRow.length;
+
+		if (this.y == 0) {
+			this.matriz[0] = newRow.length > 0 ? newRow : [ '0' ];
+			this.valFila = [];
+			return;
+		}
+
+		if (newRow.length > this.y) {
+			this.matriz = this.matriz.map((fila) => {
+				return this.fillArray(fila, long);
+			});
+		} else if (newRow.length < this.y) {
+			newRow = this.fillArray(newRow, this.y);
+		}
+		this.matriz.push(newRow);
+		this.valFila = [];
+	}
+
+	fillArray(array: Array<string>, long: number): Array<string> {
+		let arrayLength = array.length;
+		for (let index = 0; index < Math.abs(long - arrayLength); index++) {
+			array.push('0');
+		}
+		return array;
+	}
+
+	deleteData() {
+		this.valFila = [];
+		this.matriz = [ [] ];
+		this.matrices = [];
+	}
+
+	addMatriz() {
+		this.matrices.push(this.matriz);
+		this.matriz = [ [] ];
+		console.log(this.matrices);
+	}
 }
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ChipsModule } from 'primeng/chips';
+import { ButtonModule } from 'primeng/button';
+import { TabMenuModule } from 'primeng/tabmenu';
+import { FieldsetModule } from 'primeng/fieldset';
+import { CardModule } from 'primeng/card';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [ AppComponent ],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		FormsModule,
+		ChipsModule,
+		ButtonModule,
+		TabMenuModule,
+		FieldsetModule,
+		BrowserAnimationsModule,
+		CardModule
+	],
+	providers: [],
+	bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
